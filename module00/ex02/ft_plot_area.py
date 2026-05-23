3231c38528f2e383db499157f71254f9009feaf1
txavier <tchiadeestevaox@gmail.com>
2026-05-23 18:00:02 +0100

    in this exercise i learned that there is not primitive types on python, only objects kkkkkk, they really took the oop personally, the int is a fucking object, wtffffffcck

--- module00/ex02/ft_plot_area.py
+++ module00/ex02/ft_plot_area.py
@@ -1,5 +1,5 @@
 def ft_plot_area():
-	x = int (input("Enter lenght: "))
-	y = int (input("Enter width: "))
+	x = int(input("Enter lenght: "))
+	y = int(input("Enter width: "))
 	
 	print(f"Plot area: {x * y}")
