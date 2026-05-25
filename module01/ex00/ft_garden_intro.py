48245b3906bc64ebbd27091256e0f3635ec4b34b
txavier <tchiadeestevaox@gmail.com>
2026-05-25 13:53:48 +0100

    wtfck it could be executed as a bin git add ex00/ft_garden_intro.py git add ex00/ft_garden_intro.py git add ex00/ft_garden_intro.py git add ex00/ft_garden_intro.py git add ex00/ft_garden_intro.py git add ex00/ft_garden_intro.py ! holly shiiiitttttt

---
+++ module01/ex00/ft_garden_intro.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def	main():
+	print("=== Welcome to My Garden ===")
+	print("Plant: Rose\nHeigh: 25cm\nAge: 30 days")
+	print("\n=== End of Program ===")
+
+if __name__ == "__main__":
+	main()
