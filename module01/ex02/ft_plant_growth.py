0d267be2f4add22a54c25ca54007dca66a9b00de
txavier <tchiadeestevaox@gmail.com>
2026-06-15 18:47:27 +0100

    refatorei a classe plant, modifiquei o construtor e o método grow

--- module01/ex02/ft_plant_growth.py
+++ module01/ex02/ft_plant_growth.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
 class	Plant:
-	def __init__(self, name, heigh, age):
+	def __init__(self, name, heigh, age, plusAge, plusCm):
 		self.name = name
 		self.heigh = heigh
 		self.age = age
+		self.plusAge = plusAge
+		self.plusCm = plusCm
 	def show(self):
 		print(f"{self.name}: {self.heigh}cm, {self.age} days old")
 
-	def grow(self):
-		print(f"{self.name}: {self.heigh + 0.8}cm, {self.age + 1} days old")
+	def grow(self, time):
+		for i in range(time):
+			print(f"=== Day {i + 1} ===")
+			self.age += self.plusAge
+			self.heigh += self.plusCm
+			print(f"{self.name}: {self.heigh:.1f}cm, {self.age} days old")
 
 def	main():
+	rose = Plant("Rose", 25.0, 30, 1, 0.8)
+	print("=== Garden Plant Growth ===")
+	rose.show()
+	days = 7
+	rose.grow(days)
+	print(f"Growth this week: {rose.plusCm * days:.1f}cm")
 	
+
+if __name__ == "__main__":
+	main()
