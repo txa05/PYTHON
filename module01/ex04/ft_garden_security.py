6a08704170c396c0d69836f00da1a71c99fb009c
txavier <tchiadeestevaox@gmail.com>
2026-07-08 15:20:55 +0100

    refactor Plant class to use consistent naming conventions and improve encapsulation

--- module01/ex04/ft_garden_security.py
+++ module01/ex04/ft_garden_security.py
@@ -2,43 +2,60 @@
 
 class	Plant:
 	def __init__(self, name, age, height):
-		self.name = name
-		self.heigh = height
+		self._name = name
+		self._height = height
 		self._age = age
-		self.plusAge = 1
-		self.plusCm = 0.8
+		self._plusAge = 1
+		self._plusCm = 0.8
 	def show(self):
-		print(f"{self.name}: {round(self.heigh, 1)}cm, {self._age} days old")
+		print(f"{self._name}: {round(self._height, 1)}cm, {self._age} days old")
 
 	def grow(self):
-			self.heigh += self.plusCm
+			self._height += self._plusCm
 	def age(self):
-			self._age += self.plusAge
+			self._age += self._plusAge
+	
 	def set_age(self, age):
+		if (age < 0):
+			print(f"{self._name}: Error, age can't be negative\nAge update rejected")
+			return
 		self._age = age
-	def set_heigh(self, heigh):
-		self.heigh = heigh
+
+	def set_heigh(self, height):
+		if (height < 0):
+			print(f"{self._name}: Error, height can't be negative\nHeight  update rejected")
+			return
+		self._height = height
 	def get_age(self):
 		return self._age
 	def get_heigh(self):
-		return self.heigh
+		return self._height
 	def addAge(self, plusAge):
-		self.plusAge = plusAge
+		self._plusAge = plusAge
 	def addHeigh(self, plusCm):
-		self.plusCm = plusCm
+		self._plusCm = plusCm
 
 def	main():
 	print("=== Print Factory Output ===")
-	rose = Plant("Rose", 30, 25.0)
-	oak = Plant("Oak", 365, 200.0)
-	cactos = Plant("Cactos", 90, 5.0)
-	sunflower = Plant("Sunflower", 45, 80.0)
-	fern = Plant("Fern", 120, 15.0)
+	rose = Plant("Rose", 10, 15.0)
+	
+	print(f"Plant created:", end=' ')
+	rose.show()
 
-	Plantas = [rose, oak, cactos, sunflower, fern]
+	rose.set_age(30)
+	rose.set_heigh(25.0)
 
-	for plant in Plantas:
-		print("Created:", end=' ')
-		plant.show()
+	print("")
+
+	print(f"Height updated: {rose.get_heigh()}cm")
+	print(f"Age updated: {rose.get_age()} days")
+
+	print("")
+	rose.set_age(-2)
+	rose.set_heigh(-4)
+
+	print("Current state:", end=' ')
+	rose.show()
+
 if __name__ == "__main__":
 	main()
