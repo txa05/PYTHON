86dd4dfb4e4de750675ea98f5a9e7bb14a8bb6a6
tchiade xavier <tchiadeestevaox@gmail.com>
2026-07-08 08:19:17 +0100

    add ft_plant_factore.py to implement Plant class with growth and age management features

---
+++ module01/ex03/ft_plant_factore.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+class	Plant:
+	def __init__(self, name, age, height):
+		self.name = name
+		self.heigh = height
+		self._age = age
+		self.plusAge = 1
+		self.plusCm = 0.8
+	def show(self):
+		print(f"{self.name}: {round(self.heigh, 1)}cm, {self._age} days old")
+
+	def grow(self):
+			self.heigh += self.plusCm
+	def age(self):
+			self._age += self.plusAge
+	def setAge(self, age):
+		self._age = age
+	def setHeigh(self, heigh):
+		self.heigh = heigh
+	def addAge(self, plusAge):
+		self.plusAge = plusAge
+	def addHeigh(self, plusCm):
+		self.plusCm = plusCm
+
+def	main():
+	print("=== Print Factory Output ===")
+	rose = Plant("Rose", 30, 25.0)
+	oak = Plant("Oak", 365, 200.0)
+	cactos = Plant("Cactos", 90, 5.0)
+	sunflower = Plant("Sunflower", 45, 80.0)
+	fern = Plant("Fern", 120, 15.0)
+
+	Plantas = [rose, oak, cactos, sunflower, fern]
+
+	for plant in Plantas:
+		print("Created:", end=' ')
+		plant.show()
+if __name__ == "__main__":
+	main()
