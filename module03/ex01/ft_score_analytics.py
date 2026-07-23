8f8978397b6fb386af5cae0b3ed573e35dfad5eb
txavier <tchiadeestevaox@gmail.com>
2026-07-23 19:35:50 +0100

    ex01 done

--- module03/ex01/ft_score_analytics.py
+++ module03/ex01/ft_score_analytics.py
@@ -6,7 +6,35 @@ def	score_analytics() -> None:
 	if len(sys.argv) == 1:
 		print(f"No score provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
 		return
-	scores = [int(n) for n in sys.argv[1:]]
-	print(f"Scores processed:", scores)
+	scores = []
+	invalid_count = 0
+	
+	for item in sys.argv[1:]:
+		try:
+			nbr = int(item)
+			scores.append(nbr)
+		except:
+			invalid_count += 1
 
-score_analytics()
+	if invalid_count == len(sys.argv[1:]):
+		for value in sys.argv[1:]:
+			print(f"Invalid parameter: '{value}'")
+	if len(scores) == 0:
+		print(f"No score provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+		return
+	print(f"Scores processed: {scores}")
+	print(f"Total Players: {len(scores)}")
+	print(f"Total score: {sum(scores)}")
+	print(f"Average score: {sum(scores) / len(scores)}")
+	print(f"High score: {max(scores)}")
+	print(f"Low score: {min(scores)}")
+	print(f"Score range: {max(scores) - min(scores)}")
+	print("")
+
+def	main() -> None:
+	print("=== Player Score Analytics ===")
+	score_analytics()
+
+if __name__ == "__main__":
+	main()	
+	
